59b5333e67e22ed2af82cf4a5030038e1cae921f
watsonryan <rwatso12@gmail.com>
2017-06-02 17:43:44 -0400

    fixed typo

--- sims/pyDir/run.py
+++ sims/pyDir/run.py
@@ -12,27 +12,45 @@ import os, glob, subprocess, progressbar
 import matplotlib.pyplot as plt
 
 
-kernel = 'huber'
-kernelWindow = 1000
+kernel = ['huber','tukey']
+maxWidth = 2
+kernelIncrement = 0.01
 outputFile = 'huberTest.txt'
-inputFile = '../../poseGraphs/manhattanOlson3500.g2o'
+inputFile = '../../poseGraphs/intel.g2o'
 script = './../../gtsam/build/examples/processG2O'
 
 index = []
-totalError = []
+totalError1 = []
+totalError2 = []
 
 progress = progressbar.ProgressBar()
 print('\n\n')
-for k in progress(range(1, kernelWindow)):
+for k in progress(range(1, int(maxWidth/kernelIncrement))):
 
-    kernelWidth = str(float(k)/kernelWindow)
+    kernelWidth = str(float(k)*kernelIncrement)
     cmd = ['./../../gtsam/build/examples/processG2O', '-i', inputFile, 
-        '-k', kernel, '-w', kernelWidth]
+        '-k', kernel[0], '-w', kernelWidth]
 
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = proc.communicate()
+    cmd2 = ['./../../gtsam/build/examples/processG2O', '-i', inputFile, 
+        '-k', kernel[1], '-w', kernelWidth]
+
+    proc1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc2 = subprocess.Popen(cmd2, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out1, err = proc1.communicate()
+    out2, err = proc2.communicate()
     index.append( float(kernelWidth) )    
-    totalError.append( out )
+    totalError1.append( out1 )
+    totalError2.append( out2 )
 
-plt.plot(index, totalError)
+print totalError1
+print totalError2
+plt.plot(index, totalError1, 'k',label='Huber', linewidth=3.0)
+plt.plot( index, totalError2, 'b', label='Tukey', linewidth=3.0)
+plt.ylabel('Final Graph Error')
+plt.xlabel('Kernel Width')
+plt.grid()
+plt.autoscale(tight=True)
+font = { 'weight' : 'bold', 'size'   : 22}
+plt.rc('font', **font)
+plt.legend()
 plt.show()
